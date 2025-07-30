adbb49ee947d677e839c0354ee063df40fd13d57
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-30 22:18:35 +0545

    demo practice

--- dom/test.js
+++ dom/test.js
@@ -63,37 +63,39 @@
 
 // 4.Multi button change background
 
-const background = document.querySelector("body");
-const redBtn = document.querySelector(".red");
-const blueBtn = document.querySelector(".blue");
-const yellowBtn = document.querySelector(".yellow");
-const orangeBtn = document.querySelector(".orange");
-const brownBtn = document.querySelector(".brown");
-const blackBtn = document.querySelector(".black");
-redBtn.addEventListener("click", function () {
-  background.style.backgroundColor = "red";
-  redBtn.style.backgroundColor = "red";
-});
-blueBtn.addEventListener("click", function () {
-  background.style.backgroundColor = "blue";
-  blueBtn.style.backgroundColor = "blue";
-});
-yellowBtn.addEventListener("click", function () {
-  background.style.backgroundColor = "yellow";
-  yellowBtn.style.backgroundColor = "yellow";
-});
+// const background = document.querySelector("body");
+// const redBtn = document.querySelector(".red");
+// const blueBtn = document.querySelector(".blue");
+// const yellowBtn = document.querySelector(".yellow");
+// const orangeBtn = document.querySelector(".orange");
+// const brownBtn = document.querySelector(".brown");
+// const blackBtn = document.querySelector(".black");
+// redBtn.addEventListener("click", function () {
+//   background.style.backgroundColor = "red";
+//   redBtn.style.backgroundColor = "red";
+// });
+// blueBtn.addEventListener("click", function () {
+//   background.style.backgroundColor = "blue";
+//   blueBtn.style.backgroundColor = "blue";
+// });
+// yellowBtn.addEventListener("click", function () {
+//   background.style.backgroundColor = "yellow";
+//   yellowBtn.style.backgroundColor = "yellow";
+// });
 
-orangeBtn.addEventListener("click", function () {
-  background.style.backgroundColor = "orange";
-  orangeBtn.style.backgroundColor = "orange";
-});
-brownBtn.addEventListener("click",function(){
-   background.style.backgroundColor = "brown";
-  brownBtn.style.backgroundColor = "brown";
+// orangeBtn.addEventListener("click", function () {
+//   background.style.backgroundColor = "orange";
+//   orangeBtn.style.backgroundColor = "orange";
+// });
+// brownBtn.addEventListener("click",function(){
+//    background.style.backgroundColor = "brown";
+//   brownBtn.style.backgroundColor = "brown";
   
-})
-blackBtn.addEventListener("click",function(){
-   background.style.backgroundColor = "black";
-  blackBtn.style.backgroundColor = "black";
-  blackBtn.style.color = "white";
-})
+// })
+// blackBtn.addEventListener("click",function(){
+//    background.style.backgroundColor = "black";
+//   blackBtn.style.backgroundColor = "black";
+//   blackBtn.style.color = "white";
+// })
+
+// 5. Calculator 
